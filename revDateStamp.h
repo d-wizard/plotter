de18eccf97130c7f5acb6c5969a47592b69bfa30
d-wizard <dw.wizard@gmail.com>
2017-01-08 17:49:57 -0600

    Add auto type c header file save type.
    Original Commit Rev: 2155 | 2017-01-08 17:49:57 -0600 (Sun, 08 Jan 2017)

--- revDateStamp.h
+++ revDateStamp.h
@@ -1,4 +1,4 @@
-/* Copyright 2015 - 2016 Dan Williams. All Rights Reserved.
+/* Copyright 2015 - 2017 Dan Williams. All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this
  * software and associated documentation files (the "Software"), to deal in the Software
@@ -16,4 +16,4 @@
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
-#define REV_DATE_STAMP "Rev: 2124 - Date: 12/01/16"
+#define REV_DATE_STAMP "Rev: 2154 - Date: 01/08/17"
