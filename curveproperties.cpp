41f34d4b363f689dee9215b249874f43ad0e90fd
d-wizard <dw.wizard@gmail.com>
2021-01-21 17:02:29 -0600

    Set color back to black if no curves exist.
    Original Commit Rev: 2945 | 2021-01-21 17:02:29 -0600 (Thu, 21 Jan 2021)

--- curveproperties.cpp
+++ curveproperties.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2014 - 2020 Dan Williams. All Rights Reserved.
+/* Copyright 2014 - 2021 Dan Williams. All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this
  * software and associated documentation files (the "Software"), to deal in the Software
@@ -1721,6 +1721,7 @@ void curveProperties::fillInPropTab(bool userChangedPropertiesGuiSettings)
        ui->txtPropYMax->setText("");
        ui->propParentCurves->clear();
        ui->txtLastIp->setText("");
+       setPropTabCurveColor(Qt::black);
    }
 }
 
