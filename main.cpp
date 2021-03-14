645f888ec8e9c05e960e84232bf8c43389d97037
d-wizard <dw.wizard@gmail.com>
2021-03-13 18:27:46 -0600

    Improve performance on High Resolution Displays.
    Original Commit Rev: 3017 | 2021-03-13 18:27:46 -0600 (Sat, 13 Mar 2021)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2013 - 2019 Dan Williams. All Rights Reserved.
+/* Copyright 2013 - 2019, 2021 Dan Williams. All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this
  * software and associated documentation files (the "Software"), to deal in the Software
@@ -371,6 +371,7 @@ void restorePathsFromCmdLineInThisInstance()
 
 int main(int argc, char *argv[])
 {
+   QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
    processCmdLineArgs(argc, argv);
    processIniFile(argc, argv);
 
